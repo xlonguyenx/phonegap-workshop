c01a362043adbe1de2eba824a59bb0837d7ed61e
Long Nguyen <longuyen@adobe.com>
2015-01-09 15:33:22 -0500

    part4

--- www/js/main.js
+++ www/js/main.js
@@ -9,7 +9,6 @@ var app = {
     },
 
     findByName: function() {
-        console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
             var e;
@@ -21,12 +20,22 @@ var app = {
         });
     },
 
+    renderHomeView: function() {
+        var html =
+                "<div class='header'><h1>Home</h1></div>" +
+                "<div class='search-view'>" +
+                "<input class='search-key'/>" +
+                "<ul class='employee-list'></ul>" +
+                "</div>"
+        $('body').html(html);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };

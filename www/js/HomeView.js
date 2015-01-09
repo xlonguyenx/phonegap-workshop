0852f0c137eec10e23f04f151a8adfbc6fe4d9fe
Long Nguyen <longuyen@adobe.com>
2015-01-09 16:33:31 -0500

    part8

--- www/js/HomeView.js
+++ www/js/HomeView.js
@@ -12,16 +12,8 @@ var HomeView = function(store) {
     };
 
     this.findByName = function() {
-        var self = this;
         store.findByName($('.search-key').val(), function(employees) {
             $('.employee-list').html(HomeView.liTemplate(employees));
-            if (self.iscroll) {
-                console.log('Refresh iScroll');
-                self.iscroll.refresh();
-            } else {
-                console.log('New iScroll');
-                self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
-            }
         });
     };
 
